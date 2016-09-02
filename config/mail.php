14a89dd9c12daf4b8af5f1b4ca672340b0a90082
hitman <944594252@qq.com>
2016-09-02 18:12:18 +0800

    密码重置功能

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'service@tonglei.cn', 'name' => 'Long1390'],
 
     /*
     |--------------------------------------------------------------------------
